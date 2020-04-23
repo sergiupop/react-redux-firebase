c58613ac1e1598be3fae6c0be99a4e234c888de1
Sergiu Pop <sergiupop94@gmail.com>
2020-04-23 18:04:13 +0300

    Add redux thunk

--- firebaseapp/src/index.js
+++ firebaseapp/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 import rootReducer from './store/reducers/rootReducer';
 
 import './index.css';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
--- firebaseapp/src/store/reducers/projectReducer.js
+++ firebaseapp/src/store/reducers/projectReducer.js
@@ -7,6 +7,14 @@ const initState = {
 };
 
 const projectReducer = (state = initState, action) => {
+    switch (action.type) {
+        case 'CREATE_PROJECT':
+            console.log("Project: ", action.project);
+            break;
+        
+        default: break;
+    }
+
     return state;
 };
 
