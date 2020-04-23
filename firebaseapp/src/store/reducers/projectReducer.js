11e880778d9e43b0ed1001d8582aec5247c1dd82
Sergiu Pop <sergiupop94@gmail.com>
2020-04-23 17:43:41 +0300

    Add projects dummy data

--- firebaseapp/src/store/reducers/projectReducer.js
+++ firebaseapp/src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {};
+const initState = {
+    projects: [
+        { id: '1', title: 'Project 1', content: 'Content1' },
+        { id: '2', title: 'Project 2', content: 'Content2' },
+        { id: '3', title: 'Project 3', content: 'Content3' }
+    ]
+};
 
 const projectReducer = (state = initState, action) => {
     return state;
