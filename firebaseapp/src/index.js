b66ecefabbfd9ca8797c6061f4c9f358b8ffbcb1
Sergiu Pop <sergiupop94@gmail.com>
2020-04-23 17:32:07 +0300

    Integrate redux

--- firebaseapp/src/index.js
+++ firebaseapp/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import App from './App';
+import rootReducer from './store/reducers/rootReducer';
+
+import './index.css';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
