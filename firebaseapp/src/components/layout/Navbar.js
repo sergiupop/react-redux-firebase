cb2a13866da0439fb344746a6d02baae353e9dca
Sergiu Pop <sergiupop94@gmail.com>
2020-04-28 17:19:50 +0300

    Conditionally display navbar links based on user authentication

--- firebaseapp/src/components/layout/Navbar.js
+++ firebaseapp/src/components/layout/Navbar.js
@@ -4,13 +4,15 @@ import { Link } from 'react-router-dom';
 import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const { auth } = props;
+    const links = auth.uid ? <SignedInLinks/> : <SignedOutLinks/>;
+
     return (
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
                 <Link to='/' className="brand-logo">My firebase app</Link>
-                <SignedInLinks/>
-                <SignedOutLinks/>
+                { links }
             </div>
         </nav>
     )
@@ -18,7 +20,7 @@ const Navbar = () => {
 
 const mapStateToProps = (state) => {
     return {
-
+        auth: state.firebase.auth
     }
 }
 
