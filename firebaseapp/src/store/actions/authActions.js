e901272a30e31177913cce8797ecb8cd75124fa9
Sergiu Pop <sergiupop94@gmail.com>
2020-05-01 19:57:13 +0300

    Implement sign up logic

--- firebaseapp/src/store/actions/authActions.js
+++ firebaseapp/src/store/actions/authActions.js
@@ -21,7 +21,29 @@ export const signOut = () => {
         firebase.auth().signOut().then(() => {
             dispatch({ type: 'SIGNOUT_SUCCESS' });
         }).catch((err) => {
-            dispatch({ type: 'SINGOUT_ERROR' });
+            dispatch({ type: 'SINGOUT_ERROR', err });
         })
+    }
+}
+
+export const signUp = (newUser) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
+        const firebase = getFirebase();
+        const firestore = getFirestore();
+
+        firebase.auth().createUserWithEmailAndPassword(
+            newUser.email,
+            newUser.password
+        ).then((data) => {
+            return firestore.collection('users').doc(data.user.uid).set({
+                firstName: newUser.firstName,
+                lastName: newUser.lastName,
+                initials: newUser.firstName[0] + newUser.lastName[0]
+            })
+        }).then(() => {
+            dispatch({ type: 'SIGNUP_SUCCESS' });
+        }).catch((err) => {
+            dispatch({ type: 'SIGNUP_ERROR', err });
+        });
     }
 }
--- firebaseapp/src/store/reducers/authReducer.js
+++ firebaseapp/src/store/reducers/authReducer.js
@@ -13,7 +13,19 @@ const authReducer = (state = initState, action) => {
         case 'LOGIN_ERROR':
             return {
                 ...state,
-                authError: 'Login failed'
+                authError: action.err.message
+            }
+        
+        case 'SIGNUP_SUCCESS':
+            return {
+                ...state,
+                authError: null
+            }
+
+        case 'SIGNUP_ERROR':
+            return {
+                ...state,
+                authError: action.err.message
             }
 
         case 'SIGNOUT_SUCCESS':
@@ -25,8 +37,8 @@ const authReducer = (state = initState, action) => {
         case 'SIGNOUT_ERROR':
             return {
                 ...state,
-                authError: 'Signout failed'
-            }
+                authError: action.err.message
+            }        
 
         default: 
             return state;
