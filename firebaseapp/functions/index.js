d28982b69748a33adbb052c8a43320315a0678dc
Sergiu Pop <sergiupop94@gmail.com>
2020-05-02 16:45:58 +0300

    Create notification on project creation

--- firebaseapp/functions/index.js
+++ firebaseapp/functions/index.js
@@ -1,9 +1,26 @@
 const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello, guys!");
 });
+
+const createNotification = (notification) => {
+    return admin.firestore().collection('notifications')
+        .add(notification)
+        .then(doc => console.log('Notification add', doc))
+}
+
+exports.projectCreated = functions.firestore
+    .document('projects/{projectId}')
+    .onCreate(doc => {
+        const project = doc.data();
+        const notification = {
+            content: 'Added a new project',
+            user: `${project.authorFirstName} ${project.authorLastName}`,
+            time: admin.firestore.FieldValue.serverTimestamp()
+        }
+
+        return createNotification(notification);
+    });
